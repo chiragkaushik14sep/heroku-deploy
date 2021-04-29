ae45b83084489f15237363f237c328b2923b31aa
chiragkaushik14sep <41909231+chiragkaushik14sep@users.noreply.github.com>
2021-04-29 18:06:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
-from sklearn.linear_model import LinearRegression
-regressor = LinearRegression()
+
 app = Flask(__name__)
-# model = pickle.load(open('model.pkl', 'rb'))
-# print('model',model)
+model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def home():
@@ -17,14 +15,10 @@ def predict():
     For rendering results on HTML GUI
     '''
     int_features = [int(x) for x in request.form.values()]
-    print("int_features",int_features)
     final_features = [np.array(int_features)]
-    print("final_features",final_features)
     prediction = model.predict(final_features)
-    print(prediction)
 
     output = round(prediction[0], 2)
-    print("output",output)
 
     return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
 
