294220160aaf40db88625773f32eb2d7e28c998d
chiragkaushik14sep <41909231+chiragkaushik14sep@users.noreply.github.com>
2021-04-29 17:56:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
-print(model)
+print('model',model)
 
 @app.route('/')
 def home():
